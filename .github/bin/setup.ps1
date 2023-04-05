169945d7495406a94f2214d96e5dd023e13ce77a
kwzr <kwzr.poyo@gmail.com>
2023-04-05 09:33:51 +0900

    checkoutするdepot_toolsを変更

--- .github/bin/setup.ps1
+++ .github/bin/setup.ps1
@@ -8,6 +8,6 @@
 New-Item depot_tools -ItemType directory
 Set-Location depot_tools
 git init
-git fetch --depth=1 https://chromium.googlesource.com/chromium/tools/depot_tools.git 68377adad5aecf06c5f7e189319a5199fde6992f
+git fetch --depth=1 https://chromium.googlesource.com/chromium/tools/depot_tools.git 0ba2fd429dd6db431fcbee6995c1278d2a3657a0
 git checkout FETCH_HEAD
 Write-Output "$pwd" | Out-File -FilePath $env:GITHUB_PATH -Encoding utf8 -Append
